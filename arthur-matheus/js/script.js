215e55541a7bccf43d7979044dfdd7ad99f710ef
arthur matheus freitas da silva <131968138+Arthurprocuracasadas@users.noreply.github.com>
2023-05-17 09:12:09 -0300

    atividade de hoje 17/05/2023

---
+++ arthur-matheus/js/script.js
@@ -0,0 +1,21 @@
+
+
+function clickBotao(){
+    //alert("AQUIII O JS TA FUNCIONANDO")
+
+    let texto = document.getElementById('text-nome')
+    console.log(texto);
+    texto.className= 'texto-legal'
+
+    let texto2 = document.getElementById('text-nome2')
+    console.log(texto2);
+    texto2.className= 'texto-sentimental'
+
+    let texto3 = document.getElementById('text-nome3')
+    console.log(texto3);
+    texto3.className= 'texto-estranho'
+
+    let texto3 = document.getElementById('text-nome3')
+    console.log(texto3);
+    texto3.className= 'texto-estranho'
+}
